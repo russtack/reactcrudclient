4a061c07ef8a9fa18e289befb72842dff4fe0f43
russtack <stirlingest@gmail.com>
2023-05-19 02:37:04 -0400

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/update" element={<Update />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Books.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+const Books = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  });
+  return <div>Books</div>;
+};
+
+export default Books;
