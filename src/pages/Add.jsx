1f842b34bc5f762c93a5295d305810bb959c6eda
russtack <stirlingest@gmail.com>
2023-05-19 04:28:24 -0400

    set

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
 const Add = () => {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  return (
+    <div className="form">
+      <h1>Add new book</h1>
+      <input type="text" name="title" id="" onChange={handleChange} />
+      <input type="text" name="desc" id="" onChange={handleChange} />
+      <input type="number" name="price" id="" onChange={handleChange} />
+      <input type="text" name="cover" id="" onChange={handleChange} />
+    </div>
+  );
 };
 
 export default Add;
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 const Books = () => {
   const [books, setBooks] = useState([]);
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
+        const books = await axios.get("http://localhost:8800/books");
+        setBooks(books.data);
       } catch (err) {
         console.log(err);
       }
     };
-  });
-  return <div>Books</div>;
+    fetchAllBooks();
+  }, []);
+  return (
+    <div>
+      <h1>Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add new book</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Books;
